a06df84b33081b12da3e5448ebf9eca37c7e7a93
emirba06 <barrionuevojuan305@gmail.com>
2025-10-13 01:00:44 -0300

    migrate a sql

---
+++ proyecto/gestor_estudiantes/apps/estudiante/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Estudiante(models.Model):
+    nombre_estudiante = models.TextField(max_length=250, unique=True)
+    apellido_estduiante = models.TextField(max_length=250, unique=True)
+    edad_estdudiante = models.IntegerField(unique=True)
+    nota_curso_estudiante = models.IntegerField(blank=True)
+    curso_estudiante = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'Nombre:{self.nombre_estudiante}, Apellido:{self.apellido_estduiante},Edad:{self.edad_estdudiante},Nota:{self.nota_curso_estudiante},Curso:{self.curso_estudiante}'
+
+
+class Curso(models.Model):
+    nombre_curso = models.TextField(max_length=250, unique=True)
+    cantidad_horas_curso = models.TextField(unique=True)
+
+    def __str__(self):
+        return f'Nombre Curso:{self.nombre_curso}, Cantidad de horas:{self.cantidad_horas_curso}'
+# Create your models here.
+
+
